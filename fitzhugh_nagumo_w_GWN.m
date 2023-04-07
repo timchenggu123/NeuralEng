20843f85b7c3eac1ebf09e4a13806e31a9901d6a
Tim Gu <2013tim.g@gmail.com>
2023-04-07 16:08:00 -0400

    using spline interpolation for GWN

--- fitzhugh_nagumo_w_GWN.m
+++ fitzhugh_nagumo_w_GWN.m
@@ -4,11 +4,13 @@ function out=fitzhugh_nagumo_w_GWN(t,xy,alpha , w2, a, b, c, k1, k2, GWN, GWN_fr
     y = xy(2);
     x1 = xy(3);
     y1 = xy(4);
-    t_samp = floor(t/(1/GWN_freq)) + 1;
+    t_samp_i= floor(t/(1/GWN_freq)) + 1;
     if t_samp > length(GWN)
         t_samp=t_samp - 1;
     end
-    noise = GWN(t_samp);
+    noise_i = GWN(t_samp_i);
+    noise_j = GWN(t_samp_i + 1);
+    noise = spline([t_samp_i, noise_i], [t_samp_i + 1, noise_j], t);
     out = zeros(4,1);
     out(1) = alpha*(y + x - (x^3)/3 + (k1 + c*x1)+noise);
     out(2) = -(1/alpha) * (w2*x - a + b*y);
